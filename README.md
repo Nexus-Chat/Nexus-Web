1f2aa8c74a7d3a1b7760485869ffb3af9d721655
Taha Dostifam <mr.tahadostifam@gmail.com>
2022-07-20 19:21:53 +0430

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Nexus Web
+
+Developed using Vue and Vite.
+
+## Deploy
+
+### Development
+
+```bash
+yarn & yarn dev
+```
+
+### Production
+
+```bash
+yarn & yarn build
+yarn add http-server
+http-server -p 3000 .
+```
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Ref, ref } from "vue";
+
+var count: Ref<number> = ref(1);
+
+function nextCount(e: Event) {
+  count.value++;
+  (e.target as HTMLButtonElement).innerHTML = `Count:-> ${count.value}`;
+
+  if (count.value == 5) {
+    (e.target as HTMLButtonElement).style.background = "red";
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button @click="nextCount">1</button>
+  </div>
+</template>
